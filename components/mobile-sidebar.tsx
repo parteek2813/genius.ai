011e93859f4a8c9962808df0c8c1ba90d58eb931
parteek kumar <digitalparteek@gmail.com>
2023-08-18 18:12:59 +0530

    mobile sidebar added

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Menu, Sidebar } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,17 @@
-import { Button } from "@/components/ui/button"
-import { UserButton } from "@clerk/nextjs"
-import {Menu} from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-        <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu />
-        </Button>
-        <div className="flex w-full justify-end ">
-            <UserButton afterSignOutUrl="/"/>
-        </div>
+      <MobileSidebar />
+      <div className="flex w-full justify-end ">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
