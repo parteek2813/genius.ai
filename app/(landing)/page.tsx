ecf4623c18045613eaf09bc08fea9b29eeb21bee
parteek kumar <digitalparteek@gmail.com>
2023-08-30 08:34:33 +0530

    landing page setup

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,16 @@
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const LandingPage = () => {
-    return (
-        <div>
-            Landing page (unprotected)
-            <div>
-                <Link href="sign-in">
-                <Button>
-                    Login
-                </Button>
-                </Link>
-                <Link href="sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      {/* <LandingContent /> */}
+    </div>
+  );
+};
 
 export default LandingPage;
